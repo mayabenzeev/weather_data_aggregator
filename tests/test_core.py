d409f78e719d728744be239a92863dd735ba9319
Maya Benzeev <maya.benzeev@post.runi.ac.il>
2025-06-02 14:44:06 +0300

    clean

--- tests/test_core.py
+++
@@ -1,7 +0,0 @@
-def test_fetch_and_store(monkeypatch):
-    # TODO: Mock requests and test fetch_and_store
-    pass
-
-def test_get_analytics():
-    # TODO: Test analytics logic with sample data
-    pass
